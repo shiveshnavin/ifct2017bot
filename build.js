57d845eeb960ccf6c3eb88e61aeae36bb2454ff9
Subhajit Sahu <wolfram77@gmail.com>
2018-04-27 11:26:12 +0530

    :snowman: save changes

--- build.js
+++ build.js
@@ -4,12 +4,11 @@ const os = require('os');
 
 function replace(txt) {
   var z = [], mat = txt.match(/{(\w+)}/);
-  if(mat==null || !entities.has(mat[1])) return z;
+  if(mat==null || !entities.has(mat[1])) return [txt];
   for(val of entities.get(mat[1])) {
     var rtxt = txt.replace(/{\w+}/, val);
-    var atxt = replace(txt);
-    if(atxt.length===0) z.push(rtxt);
-    else z.push.apply(z, atxt);
+    var atxt = replace(rtxt);
+    z.push.apply(z, atxt);
   }
   return z;
 };
@@ -19,10 +18,11 @@ for(var fil of fs.readdirSync('entities')) {
   var nam = fil.replace('.txt', '');
   var data = fs.readFileSync(path.join('entities', fil), 'utf8');
   var values = data.trim().split(/\r?\n/g);
-  entites.set(nam, values);
+  entities.set(nam, values);
 }
 
 for(var fil of fs.readdirSync('intents')) {
+  if(!fil.endsWith('.txt')) continue;
   var nam = fil.replace('.txt', '');
   var data = fs.readFileSync(path.join('intents', fil), 'utf8');
   var values = data.trim().split(/\r?\n/g), txts = [];
