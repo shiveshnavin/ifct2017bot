8386543a2d8f561be964d602334615ef3d148467
Glitch (ifct2017bot) <none>
2018-06-05 18:59:11 +0000

    📅🍏 bringing in a few global variable, still thinking about modes

--- fn/data.js
+++ fn/data.js
@@ -1,13 +1,51 @@
+/* global Map Set */
+require('number-extra');
 const Sql = require('sql-extra');
 const ifct2017 = require('ifct2017');
 
 const COLUMNS = ifct2017.columns.corpus;
+const EXCLUDE_DEF = ['lang', 'tags', 'tsvector'];
+const ORDER_DEF = ['code', 'name', 'scie', 'lang', 'grup', 'regn', 'enerc', 'tsvector'];
+const TYPE_DEF = new Map([
+  ['code', 'TEXT'],
+  ['name', 'TEXT'],
+  ['scie', 'TEXT'],
+  ['lang', 'TEXT'],
+  ['grup', 'TEXT'],
+  ['regn', 'INT'],
+  ['tsvector', 'TSVECTOR'],
+]);
+const UNIT_DEF = new Map([
+  ['enerc', 'kcal'],
+]);
+const UNIT_MAP = new Map([
+  [0, 'g'],
+  [3, 'mg'],
+  [6, 'ug'],
+  [9, 'ng'],
+]);
 
 
-function ansColumns(row) {
+function colFactor(col) {
+  var max = Math.max.apply(null, col);
+  return Math.min(-Math.floor(Math.log10(max+1e-10)/3)*3, 9);
+};
+
+function ansMeta(rs) {
   var z = {};
-  for(var k in row)
-    z[k] = COLUMNS.get();
+  if(rs.length===0) return z;
+  for(var k in rs[0]) {
+    if(k.endsWith('_e')) continue;
+    z[k] = {name: COLUMNS.get(k), type: TYPE_DEF.get(k)||'REAL'};
+  }
+  return z;
+};
+
+function ansColumns(rs) {
+  var z = {};
+  if(rs.length===0) return z;
+  for(var k in rs[0])
+    z[k] = rs.map(r => r[k]);
   return z;
 };
 
