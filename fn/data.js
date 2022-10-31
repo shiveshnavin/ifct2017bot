0ff004ed441179d81c41349ea10047f077b9453c
Shivesh Navin <shiveshnavin@gmail.com>
2022-10-31 14:34:12 +0530

    Added PG

--- fn/data.js
+++ fn/data.js
@@ -57,22 +57,22 @@ function replaceColumn(txt) {
 
 function mapTable(txt) {
   var stm = txt.split(' ').filter(v => !IGNORE.test(v)).map(natural.PorterStemmer.stem).sort().join(' ');
-  if(TABLE_COD.has(stm)) return [TABLE_COD.get(stm)];
+  if (TABLE_COD.has(stm)) return [TABLE_COD.get(stm)];
   return [`"tsvector" @@ plainto_tsquery('${txt}')`, 'compositions_tsvector'];
 }
 
 function mapColumn(db, txt, typ, hnt, frm) {
   var txt = replaceColumn(txt), cols = [];
-  if(COLUMN_ALL.has(natural.PorterStemmer.stem(txt))) return Promise.resolve(['*']);
-  if(!frm.includes('compositions_tsvector')) return [txt.toLowerCase()];
+  if (COLUMN_ALL.has(natural.PorterStemmer.stem(txt))) return Promise.resolve(['*']);
+  if (!frm.includes('compositions_tsvector')) return [txt.toLowerCase()];
   var sql = 'SELECT "code" FROM "columns_tsvector" WHERE "tsvector" @@ plainto_tsquery($1)';
-  if(hnt==null) sql += ' ORDER BY ts_rank("tsvector", plainto_tsquery($1), 0) DESC LIMIT 1';
+  if (hnt == null) sql += ' ORDER BY ts_rank("tsvector", plainto_tsquery($1), 0) DESC LIMIT 1';
   return db.query(sql, [txt]).then(ans => {
-    for(var r of ans.rows||[]) {
+    for (var r of ans.rows || []) {
       cols.push(r.code);
-      if(typ!=='columns') continue;
-      if(hnt!=null && hnt!=='all') continue;
-      if(!COLUMN_VAL.has(r.code)) cols.push(r.code+'_e');
+      if (typ !== 'columns') continue;
+      if (hnt != null && hnt !== 'all') continue;
+      if (!COLUMN_VAL.has(r.code)) cols.push(r.code + '_e');
     }
     return cols;
   });
@@ -80,97 +80,97 @@ function mapColumn(db, txt, typ, hnt, frm) {
 
 function mapRow(db, txt, typ, hnt, frm) {
   var sql = 'SELECT "code" FROM "compositions_tsvector" WHERE "tsvector" @@ plainto_tsquery($1)';
-  if(hnt==null) sql += ' ORDER BY ts_rank("tsvector", plainto_tsquery($1), 0) DESC LIMIT 1';
-  return db.query(sql, [txt]).then(ans => (ans.rows||[]).map(v => v.code));
+  if (hnt == null) sql += ' ORDER BY ts_rank("tsvector", plainto_tsquery($1), 0) DESC LIMIT 1';
+  return db.query(sql, [txt]).then(ans => (ans.rows || []).map(v => v.code));
 }
 
 function mapEntity(db, txt, typ, hnt, frm) {
-  if(typ==='from') return mapTable(txt);
+  if (typ === 'from') return mapTable(txt);
   else return mapColumn(db, txt, typ, hnt, frm);
 }
 
 
 function matchTable(wrds) {
   wrds = wrds.map(natural.PorterStemmer.stem);
-  for(var i=wrds.length; i>0; i--) {
+  for (var i = wrds.length; i > 0; i--) {
     var txt = wrds.filter(v => !IGNORE.test(v)).sort().join(' ');
-    if(TABLE_COD.has(txt)) return {value: TABLE_COD.get(txt), hint: TABLE_COD.get(txt), length: i};
+    if (TABLE_COD.has(txt)) return { value: TABLE_COD.get(txt), hint: TABLE_COD.get(txt), length: i };
   }
   return null;
 }
 
 function matchColumn(db, wrds) {
   var sql = '', par = [];
-  for(var i=wrds.length, p=1; i>0; i--, p++) {
+  for (var i = wrds.length, p = 1; i > 0; i--, p++) {
     sql += `SELECT "code", '${i}'::INT AS i FROM "columns_tsvector" WHERE "tsvector" @@ plainto_tsquery($${p}) UNION ALL `;
     par.push(replaceColumn(wrds.slice(0, i).join(' ')));
   }
-  sql = sql.substring(0, sql.length-11);
+  sql = sql.substring(0, sql.length - 11);
   return db.query(sql, par).then((ans) => {
-    var col = COLUMN_ALL.has(natural.PorterStemmer.stem(wrds[0]))? '*':null, ncol = col? 1:0;
-    if(ans.rowCount>0 && ans.rows[0].i>ncol) return {value: ans.rows[0].code, hint: 'compositions_tsvector', length: ans.rows[0].i};
-    return col? {value: col, length: 1}:null;
+    var col = COLUMN_ALL.has(natural.PorterStemmer.stem(wrds[0])) ? '*' : null, ncol = col ? 1 : 0;
+    if (ans.rowCount > 0 && ans.rows[0].i > ncol) return { value: ans.rows[0].code, hint: 'compositions_tsvector', length: ans.rows[0].i };
+    return col ? { value: col, length: 1 } : null;
   });
 }
 
 function matchRow(db, wrds) {
   var sql = '', par = [];
-  for(var i=wrds.length, p=1; i>0; i--, p++) {
+  for (var i = wrds.length, p = 1; i > 0; i--, p++) {
     sql += `SELECT "code", '${i}'::INT AS i FROM "compositions_tsvector" WHERE "tsvector" @@ plainto_tsquery($${p}) UNION ALL `;
     par.push(wrds.slice(0, i).join(' '));
   }
-  sql = sql.substring(0, sql.length-11);
-  return db.query(sql, par).then(ans => ans.rowCount>0? {value: ans.rows[0].code, hint: 'compositions_tsvector', length: ans.rows[0].i}:null);
+  sql = sql.substring(0, sql.length - 11);
+  return db.query(sql, par).then(ans => ans.rowCount > 0 ? { value: ans.rows[0].code, hint: 'compositions_tsvector', length: ans.rows[0].i } : null);
 }
 
 function matchEntity(db, wrds) {
   var rdy = [matchTable(wrds), matchColumn(db, wrds), matchRow(db, wrds)];
   return Promise.all(rdy).then(ans => {
-    var l = ans.map(v => v? v.length:0);
+    var l = ans.map(v => v ? v.length : 0);
     console.log(wrds, ans);
-    var mi = l[1]>l[0]? 1:0;
-    mi = l[2]>l[mi]? 2:mi;
+    var mi = l[1] > l[0] ? 1 : 0;
+    mi = l[2] > l[mi] ? 2 : mi;
     console.log(wrds, l, mi);
-    if(l[mi]===0) return null;
+    if (l[mi] === 0) return null;
     var value = wrds.slice(0, l[mi]).join(' '), hint = ans[mi].hint;
-    return {type: MATCH_TYP[mi], value, hint, length: l[mi]};
+    return { type: MATCH_TYP[mi], value, hint, length: l[mi] };
   });
 }
 
 
-function exclude(rows, re=EXCLUDE_DEF) {
-  for(var row of rows) {
-    for(var k in row)
-      if(re.test(k)) row[k] = 0;
+function exclude(rows, re = EXCLUDE_DEF) {
+  for (var row of rows) {
+    for (var k in row)
+      if (re.test(k)) row[k] = 0;
   }
 }
 
-function orderBy(cols, by, pre=ORDER_DEF) {
+function orderBy(cols, by, pre = ORDER_DEF) {
   var tcols = {}, cmp = {}, ks = [], tks = null;
-  for(var k in cols)
-    if(!k.endsWith('_e') && !pre.includes(k)) ks.push(k);
-  if(by==='max') { for(var k of ks) cmp[k] = Math.max.apply(null, cols[k]); }
-  else if(by==='min') { for(var k of ks) cmp[k] = Math.min.apply(null, cols[k]); }
-  else if(by==='sum') { for(var k of ks) cmp[k] = Array.sum(cols[k]); }
-  else if(by==='range') { for(var k of ks) cmp[k] = Math.max.apply(null, cols[k])-Math.min.apply(null, cols[k]); }
-  else if(by==='relrange') { for(var k of ks) { var max = Math.max.apply(null, cols[k]), min = Math.min.apply(null, cols); cmp[k] = max-min/max||1; } }
-  else if(by==='error') { for(var k of ks) cmp[k] = Array.sum(cols[k+'_e']); }
-  else if(by==='name') tks = ks.sort();
+  for (var k in cols)
+    if (!k.endsWith('_e') && !pre.includes(k)) ks.push(k);
+  if (by === 'max') { for (var k of ks) cmp[k] = Math.max.apply(null, cols[k]); }
+  else if (by === 'min') { for (var k of ks) cmp[k] = Math.min.apply(null, cols[k]); }
+  else if (by === 'sum') { for (var k of ks) cmp[k] = Array.sum(cols[k]); }
+  else if (by === 'range') { for (var k of ks) cmp[k] = Math.max.apply(null, cols[k]) - Math.min.apply(null, cols[k]); }
+  else if (by === 'relrange') { for (var k of ks) { var max = Math.max.apply(null, cols[k]), min = Math.min.apply(null, cols); cmp[k] = max - min / max || 1; } }
+  else if (by === 'error') { for (var k of ks) cmp[k] = Array.sum(cols[k + '_e']); }
+  else if (by === 'name') tks = ks.sort();
   else tks = ks;
-  tks = tks||ks.sort((a, b) => cmp[b]-cmp[a]);
-  for(var k of pre)
-    if(k in cols) tcols[k] = cols[k];
-  for(var k of tks)
+  tks = tks || ks.sort((a, b) => cmp[b] - cmp[a]);
+  for (var k of pre)
+    if (k in cols) tcols[k] = cols[k];
+  for (var k of tks)
     tcols[k] = cols[k];
-  for(var k in cols)
+  for (var k in cols)
     tcols[k] = cols[k];
   return tcols;
 }
 
 function describe(rows) {
-  var rows = rows||[];
+  var rows = rows || [];
   exclude(rows);
-  return {rows};
+  return { rows };
 }
 
 
@@ -178,38 +178,36 @@ async function setup(db) {
   var o = ifct2017;
   o.columns.load();
   var ans = await db.query(Sql.tableExists('compositions'));
-  if(ans.rows[0].exists) return console.log(`DATA: already setup`);
-  await Promise.all([
-    db.query(o.abbreviations.sql()),
-    db.query(o.carbohydrates.sql()),
-    db.query(o.columns.sql()),
-    db.query(o.compositingCentres.sql()),
-    db.query(o.contents.sql()),
-    db.query(o.energies.sql()),
-    db.query(o.frequencyDistribution.sql()),
-    db.query(o.groups.sql()),
-    db.query(o.jonesFactors.sql()),
-    db.query(o.languages.sql()),
-    db.query(o.methods.sql()),
-    db.query(o.regions.sql()),
-    db.query(o.samplingUnits.sql()),
+  if (ans.rows[0].exists) return console.log(`DATA: already setup`);
+  await db.query(o.abbreviations.sql()),
+    await db.query(o.carbohydrates.sql()),
+    await db.query(o.columns.sql()),
+    await db.query(o.compositingCentres.sql()),
+    await db.query(o.contents.sql()),
+    await db.query(o.energies.sql()),
+    await db.query(o.frequencyDistribution.sql()),
+    await db.query(o.groups.sql()),
+    await db.query(o.jonesFactors.sql()),
+    await db.query(o.languages.sql()),
+    await db.query(o.methods.sql()),
+    await db.query(o.regions.sql()),
+    await db.query(o.samplingUnits.sql()),
     o.codes.sql().then(ans => db.query(ans)),
     o.compositions.sql().then(ans => db.query(ans)),
     o.descriptions.sql().then(ans => db.query(ans)),
-  ]);
-  console.log(`DATA: setup done`);
+    console.log(`DATA: setup done`);
 }
 
-function data(db, txt, o={}) {
+function data(db, txt, o = {}) {
   var tab = txt.replace(/[\'\"]/g, '$1$1');
   var qry = `SELECT * FROM "${tab}" WHERE TRUE`, par = [], i = 0;
-  for(var k in o) {
-    if(k.includes('"')) continue;
-    qry += ` AND "${k}"=$${i+1}`;
+  for (var k in o) {
+    if (k.includes('"')) continue;
+    qry += ` AND "${k}"=$${i + 1}`;
     par[i++] = o[k];
   }
   console.log('DATA:', qry, par);
-  return db.query(qry, par).then(ans => ans.rows||[]);
+  return db.query(qry, par).then(ans => ans.rows || []);
 }
 
 data.setup = setup;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,20 +12,30 @@ const query = require('./fn/query');
 const E = process.env;
 var app = express();
 var server = http.createServer(app);
-var db = new pg.Pool({connectionString: E.DATABASE_URL+'?ssl=true'});
-var dq = new pg.Pool({connectionString: E.DATABASE_QUERY_URL+'?ssl=true'});
+var db = new pg.Pool({
+  connectionString: E.DATABASE_URL, keepAlive: true,
+  connectionTimeoutMillis: 10000,
+  max: 4,
+  maxUses: 4
+});
+var dq = new pg.Pool({
+  connectionString: E.DATABASE_QUERY_URL, keepAlive: true,
+  connectionTimeoutMillis: 10000,
+  max: 4,
+  maxUses: 4
+});
 
 function enableCors(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method==='OPTIONS') res.send(200);
+  if (req.method === 'OPTIONS') res.send(200);
   else next();
 };
 
 app.use(enableCors);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.all('/fn/data/:txt', (req, res, next) => data(db, req.params.txt, req.query).then(ans => res.json(ans), next));
 app.all('/fn/sql/:txt', (req, res, next) => inp.sql(db, req.params.txt).then(ans => res.json(ans), next));
@@ -32,18 +44,20 @@ app.all('/fn/english/:txt', (req, res, next) => inp.english(db, req.params.txt).
 app.all('/fn/query/search/', (req, res, next) => query.search(dq, '').then(ans => res.json(ans), next));
 app.all('/fn/query/search/:txt', (req, res, next) => query.search(dq, req.params.txt).then(ans => res.json(ans), next));
 app.all('/fn/query/save/:txt', (req, res, next) => query.save(dq, req.params.txt).then(ans => res.json(ans), next));
-app.all('/', (req, res, next) => res.redirect('https://ifct2017.github.io'));
+app.all('/', (req, res, next) => {
+  res.send('Working')
+});
 
-app.use(express.static('assets', {extensions: ['html']}));
+app.use(express.static('assets', { extensions: ['html'] }));
 app.use((err, req, res, next) => {
-  var {message} = err;
-  res.status(500).json(Object.assign({message}, err));
+  var { message } = err;
+  res.status(500).json(Object.assign({ message }, err));
   console.error(err);
 });
 
 
-server.listen(E.PORT||80, () => {
-  var {port} = server.address();
+server.listen(E.PORT || 80, () => {
+  var { port } = server.address();
   console.log(`SERVER: ready at port ${port}`);
 });
 data.setup(db);
